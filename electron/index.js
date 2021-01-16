ef199dc2404736cc7d90450109951842bd1553ff
Romila Vlad <romilavlad2001@gmail.com>
2021-01-17 01:07:04 +0200

    Done

--- electron/index.js
+++ electron/index.js
@@ -8,5 +8,8 @@ router
     .get('/', function (req, res) {
         res.sendFile(path.join(__dirname + '/public/index.html'));
     })
+    .get('/video', function (req, res) {
+        res.redirect("https://www.youtube.com/watch?v=m5fvrgFDdGQ")
+    })
 
 module.exports = router;
