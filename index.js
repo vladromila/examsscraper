a28514b305d93902565d8579ff8a21fb079eba97
Romila Vlad <romilavlad2001@gmail.com>
2021-02-06 20:07:30 +0200

    Done

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let programareSala = require('./programare-sala');
 let programareNumere = require('./programare-numere');
 let electron = require('./electron')
 var cors = require('cors');
+const { count } = require("console");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -33,7 +34,7 @@ admin.database().ref("/sala/users").on("value", users => {
                 isAtLeastOneUser = true
             }
         })
-    console.log(isAtLeastOneUser, toVerifyUsers);
+    console.log(isAtLeastOneUser)
     if (isAtLeastOneUser === true)
         interval = setInterval(() => {
             request({
@@ -58,62 +59,61 @@ admin.database().ref("/sala/users").on("value", users => {
                                         goodDates.push(d)
                                 })
                                 admin.database().ref(`/sala/${user}/dates/`).set(JSON.stringify(goodDates))
-                                console.log(goodDates);
+                                console.log(user, goodDates);
                             }
                         })
                     })
                 }
+                else
+                    console.log("Aasdasdas")
             }
             )
         }, 1000)
 })
 
-admin.database().ref("/numere/users").on("value", users => {
-    clearInterval(interval);
-    let toVerifyUsers = [];
-    let isAtLeastOneUser = false
-    console.log(users.val());
-    if (users.val())
-        Object.keys(users.val()).forEach(key => {
-            if (users.val()[key].enabled === true) {
-                console.log("nicu e prost");
-                toVerifyUsers.push(key);
-                isAtLeastOneUser = true
-            }
-        })
-    console.log(isAtLeastOneUser, toVerifyUsers);
-    if (isAtLeastOneUser === true)
-        interval = setInterval(() => {
-            request({
-                url: 'https://www.drpciv.ro/drpciv-booking-api/getAvailableDaysForSpecificService/8/22',
-                method: "GET",
-                headers: {
-                    "content-type": "application/json",
-                },
-                body: {
-                },
-                json: true
-            }, (err, res, body) => {
-                if (!err) {
-                    toVerifyUsers.forEach(user => {
-                        admin.database().ref(`/numere/${user}/date`).once("value", date => {
-                            if (date) {
-                                let selectedDate = new Date(date.val());
-                                let goodDates = [];
-                                body.forEach(d => {
-                                    if (new Date(d) <= selectedDate)
-                                        goodDates.push(d)
-                                })
-                                admin.database().ref(`/numere/${user}/dates/`).set(JSON.stringify(goodDates))
-                                console.log(goodDates);
-                            }
-                        })
-                    })
-                }
-            }
-            )
-        }, 1000)
-})
+// admin.database().ref("/numere/users").on("value", users => {
+//     clearInterval(interval);
+//     let toVerifyUsers = [];
+//     let isAtLeastOneUser = false
+//     if (users.val())
+//         Object.keys(users.val()).forEach(key => {
+//             if (users.val()[key].enabled === true) {
+//                 console.log("nicu e prost");
+//                 toVerifyUsers.push(key);
+//                 isAtLeastOneUser = true
+//             }
+//         })
+//     if (isAtLeastOneUser === true)
+//         interval = setInterval(() => {
+//             request({
+//                 url: 'https://www.drpciv.ro/drpciv-booking-api/getAvailableDaysForSpecificService/8/22',
+//                 method: "GET",
+//                 headers: {
+//                     "content-type": "application/json",
+//                 },
+//                 body: {
+//                 },
+//                 json: true
+//             }, (err, res, body) => {
+//                 if (!err) {
+//                     toVerifyUsers.forEach(user => {
+//                         admin.database().ref(`/numere/${user}/date`).once("value", date => {
+//                             if (date) {
+//                                 let selectedDate = new Date(date.val());
+//                                 let goodDates = [];
+//                                 body.forEach(d => {
+//                                     if (new Date(d) <= selectedDate)
+//                                         goodDates.push(d)
+//                                 })
+//                                 admin.database().ref(`/numere/${user}/dates/`).set(JSON.stringify(goodDates))
+//                             }
+//                         })
+//                     })
+//                 }
+//             }
+//             )
+//         }, 1000)
+// })
 
 app.use(require('body-parser').json());
 
